2870806af243783bd1a510fd1c99e9052e6b99ae
Haris Osmanagic <haris@meroxa.io>
2022-10-18 13:59:14 +0200

    fix tests

--- destination_test.go
+++ destination_test.go
@@ -10,25 +10,12 @@ import (
 func TestConfigureDestination_FailsWhenConfigEmpty(t *testing.T) {
 	con := connectorName.Destination{}
 	err := con.Configure(context.Background(), make(map[string]string))
-	if err != nil {
-		t.Errorf("expected no error, got %v", err)
+	if err == nil {
+		t.Error("expected error for missing config params")
 	}
 
 	if strings.HasPrefix(err.Error(), "config is invalid:") {
 		t.Errorf("expected error to be about missing config, got %v", err)
-	}
-}
-
-func TestConfigureDestination_FailsWhenConfigInvalid(t *testing.T) {
-	con := connectorName.Destination{}
-	err := con.Configure(context.Background(), map[string]string{"foobar": "foobar"})
-	if err != nil {
-		t.Errorf("expected no error, got %v", err)
-
-	}
-
-	if strings.HasPrefix(err.Error(), "config is missing:") {
-		t.Errorf("expected error to be about invalid config, got %v", err)
 	}
 }
 
